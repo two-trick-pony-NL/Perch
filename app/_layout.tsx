d6d6e29b1598a02e5679d0d3e7e1b7654ce7e690
Peter van Doorn <peter@petervandoorn.com>
2025-02-19 22:39:03 +0100

    added gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
