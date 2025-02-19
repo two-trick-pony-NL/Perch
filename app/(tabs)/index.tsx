662ea3e180ef85150624b8c7b6d34bf092cb97f2
Peter van Doorn <peter@petervandoorn.com>
2025-02-19 22:45:54 +0100

    added gluestack and imported components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
-import { View, StyleSheet, Text, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { HStack } from '@/components/ui/hstack';
+import { Button } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
 
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-    <View>
-      <Text>HELLO WORLD</Text>
-    </View>
+      <HStack>
+        <Text className='text-2xl font-bold'>HELLO WORLD</Text>
+    </HStack>
     </SafeAreaView>
   );
 }
